1db65d83dc93046751a617d90ada73e773ee9c79
Gabriand <gabrielrivera11a@gmail.com>
2025-03-15 19:33:25 -0500

    lanzamiento

---
+++ segundoRepo/__init__.py
@@ -0,0 +1,2 @@
+def saludar(nomnre):
+    print(f'Hola {nombre} este es mi primer paquete pip')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="segundoRepo",                             # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Gabrian",                               # Tu nombre
+    author_email="Gabrian@gmail.com",               # Tu correo electrónico
+    url="https://github.com/Gabrian",               # URL del proyecto
+)
